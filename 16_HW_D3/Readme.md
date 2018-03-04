2b28026c15d1cf54642403215e1d629d1b2d49cb
rs <29244287+bkcno@users.noreply.github.com>
2018-03-04 01:15:16 -0800

    HW#16: D3

--- 16_HW_D3/Readme.md
+++ 16_HW_D3/Readme.md
@@ -1,41 +1,21 @@
-# HW 16 | Assignment 
+# 16 | Data Journalism and D3
 
 ## Background
 
 Scatter plot to demonstrate health risks facing particular demographics, with data from the U.S. Census Bureau and the Behavioral Risk Factor Surveillance System.
 
-## Tasks
+### Tasks Include:
 
-### Level 1: D3 
-
-Find a correlation between two data variables, each measured state by state and taken from different data sources. Then visualize the correlation with a scatter plot and embed the graphic into an .html file by accomplishing the following four steps:
-
-#### 1. Find the Data
-
-The 2014 one-year estimates from the U.S. Census Bureau's American Community Survey by using the [American FactFinder](http://factfinder.census.gov/faces/nav/jsf/pages/searchresults.xhtml) tool, and the data on health risks using 2014 survey data from the [Behavioral Risk Factor Surveillance System](https://chronicdata.cdc.gov/Behavioral-Risk-Factors/BRFSS-2014-Overall/5ra3-ixqq). 
-
-#### 2. Format and Test the Data
-
-Format data for D3. With two data types chosen, grab the value columns from each and paste them into a new Excel document. Create header names that you can easily call with JavaScript (concise, lowercased, camelCased). Make sure that rows and columns line up.
-
-#### 3. Visualize the Data
+Find a correlation between two data variables, each measured state by state and taken from different data sources. Then visualize the correlation with a scatter plot and embed the graphic into an .html file. The data is one-year estimates in 2014 from the U.S. Census Bureau's American Community Survey by using the [American FactFinder](http://factfinder.census.gov/faces/nav/jsf/pages/searchresults.xhtml) tool, and the data on health risks using 2014 survey data from the [Behavioral Risk Factor Surveillance System](https://chronicdata.cdc.gov/Behavioral-Risk-Factors/BRFSS-2014-Overall/5ra3-ixqq). 
 
 Using the D3 techniques to create a scatter plot that represents each state with circle elements. Code this graphic in the `app.js` file.
 
-#### 4. Embed into an iframe
+Embed `d3.html` in `index.html` with an iframe. Add a written analysis of the data below the graphs.
 
-Embed `d3.html` in `index.html` with an iframe. Add a written analysis of the data below the graphic.
+The incorporate d3-tip to display data with each state (circle) when user hovers cusor over the elemen. 
 
+Last but not least, dynamic D3 to enable resizing the graphs for mobile users. 
 
-### Level 2: Presentation
-
-#### 1. More Data, More Dynamics
-
-#### 2. Incorporate d3-tip
-
-- - -
-
-### Level 3: Mobile Friendly
 
 
 - - -
---
+++ 16_HW_D3/assets/js/app.js
@@ -0,0 +1,167 @@
+// D3 Scatterplot 
+var svgWidth = 720;
+var svgHeight = 600;
+var margin = { top: 20, right: 40, bottom: 120, left: 100 };
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select(".chart")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var chart = svg.append("g");
+d3.csv("data/data.csv", function(err, stateData) {
+    if (err) {
+        throw err;
+    }
+    stateData.forEach(function(data) {
+        data.education = +data.education;
+        data.healthcare = +data.healthcare;
+        console.log(data)
+    });
+    // Create scale functions
+    var yLinearScale = d3.scaleLinear()
+        .range([height, 0]);
+    var xLinearScale = d3.scaleLinear()
+        .range([0, width]);
+    // Create axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+    // Scale the domain d3.extent(stateData, function(data)
+    xLinearScale.domain([5, d3.max(stateData, function(data) {
+        return +data.education;
+    })]);
+    yLinearScale.domain([0, d3.max(stateData, function(data) {
+        return +data.healthcare;
+    })]);
+   
+    var toolTip = d3
+        .tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(data) {
+            var stateName = data.state;
+            var educationLevel = +data.education;
+            var healthcareCoverage = +data.healthcare;
+            return (stateName + "<br> Education: " + educationLevel + "<br> Healthcare: " + healthcareCoverage);
+        });
+
+    chart.call(toolTip);
+
+    chart.selectAll("circle")
+        .data(stateData)
+        .enter()
+        .append("circle")
+        .attr("cx", function(data, index) {
+            return xLinearScale(data.education);
+        })
+        .attr("cy", function(data, index) {
+            return yLinearScale(data.healthcare);
+        })
+        .attr('text-anchor', "middle")
+        .attr('dy', '0.35em')
+        .text(function(data) { 
+            return data.abbr;
+        })    
+        .attr("r", "12")
+        .attr("fill", "steelblue")
+        .style("stroke", "grey")
+        .style("opacity", 0.8)
+        .on("mouseover", toolTip.show)
+        .on("mouseout", toolTip.hide);
+     
+    chart.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+    chart.append("g")
+        .call(leftAxis);
+    chart.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Lacks Healthcare (%)");
+
+    // Append x-axis labels
+    chart.append("text")
+        .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top + 30) + ")")
+        .attr("class", "axis-text active")
+        .attr("data-axis-name", "education")
+        .text("Education Level, less than high school graduate (%)");
+    
+    chart.append("text")
+        .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top + 48) + ")")
+        .attr("class", "axis-text inactive")
+        .attr("data-axis-name", "higheducation")
+        .text("Education Level, college graduate or higher (%)");    
+
+    chart.append("text")
+        .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top + 64) + ")")
+        .attr("class", "axis-text inactive")
+        .attr("data-axis-name", "poverty")
+        .text("In Poverty %");
+    
+    function labelChange(clickedAxis) {
+            d3
+                .selectAll(".axis-text")
+                .filter(".active")
+            // An alternative to .attr("class", <className>) method. Used to toggle classes.
+                .classed("active", false)
+                .classed("inactive", true);
+    
+            clickedAxis.classed("inactive", false).classed("active", true);
+        }
+    
+        function findMinAndMax(dataColumnX) {
+            return d3.extent(stateData, function(data) {
+                return +data[dataColumnX];
+            });
+        }
+    
+        d3.selectAll(".axis-text").on("click", function() {
+        // Assign a variable to current axis
+            var clickedSelection = d3.select(this);
+            // "true" or "false" based on whether the axis is currently selected
+            var isClickedSelectionInactive = clickedSelection.classed("inactive");
+            // console.log("this axis is inactive", isClickedSelectionInactive)
+            // Grab the data-attribute of the axis and assign it to a variable
+            // e.g. if data-axis-name is "poverty," var clickedAxis = "poverty"
+            var clickedAxis = clickedSelection.attr("data-axis-name");
+            console.log("current axis: ", clickedAxis);
+    
+            // The onclick events below take place only if the x-axis is inactive
+            // Clicking on an already active axis will therefore do nothing
+            if (isClickedSelectionInactive) {
+                // Set the domain for the x-axis
+                xLinearScale.domain(findMinAndMax(clickedAxis));
+                // Create a transition effect for the x-axis
+                svg
+                    .select(".x-axis")
+                    .transition()
+                // .ease(d3.easeElastic)
+                    .duration(1800)
+                    .call(bottomAxis);
+                // Select all circles to create a transition effect, then relocate its horizontal location
+                // based on the new axis that was selected/clicked
+                d3.selectAll("circle").each(function() {
+                    d3
+                        .select(this)
+                        .transition()
+                    // .ease(d3.easeBounce)
+                        .attr("cx", function(data) {
+                            return xLinearScale(+data[clickedAxis]);
+                        })
+                        .duration(1800);
+                });
+    
+                labelChange(clickedSelection);
+            }
+        });       
+});
+
+// resizing for mobile phone users
+// d3.select(window).on('resize', [name of your function])
